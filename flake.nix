0004c3963f9a99a31e0a2139fa72da8abdc214da
gang.liu <14845458+stringang@users.noreply.github.com>
2024-04-23 11:51:31 +0800

    wip home-manager
    see:
    https://github.com/nix-community/home-manager/issues/4026

--- flake.nix
+++ flake.nix
@@ -3,11 +3,21 @@
 
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
-    nix-darwin.url = "github:LnL7/nix-darwin";
-    nix-darwin.inputs.nixpkgs.follows = "nixpkgs";
+
+    flake-utils.url = "github:numtide/flake-utils";
+
+    nix-darwin = {
+      url = "github:LnL7/nix-darwin";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+
+    home-manager = {
+      url = "github:nix-community/home-manager";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
   };
 
-  outputs = inputs@{ self, nix-darwin, nixpkgs }:
+  outputs = inputs@{ self, nixpkgs, flake-utils, nix-darwin, home-manager, ...}:
   let
     configuration = { pkgs, ... }: {
       # List packages installed in system profile. To search by name, run:
@@ -42,7 +52,23 @@
     # Build darwin flake using:
     # $ darwin-rebuild build --flake .#gangliu-MacBook-Pro
     darwinConfigurations."gangliu-MacBook-Pro" = nix-darwin.lib.darwinSystem {
-      modules = [ configuration ];
+      system = "x86_64-darwin";
+      modules = [
+        # `nix-darwin` main config
+        configuration
+        # `home-manager` module config
+        home-manager.darwinModules.home-manager
+          {
+              # hardcode see: https://github.com/nix-community/home-manager/issues/4026
+              users.users."gang.liu".home = "/Users/gang.liu";
+
+              home-manager.verbose = true;
+              home-manager.backupFileExtension = "hm_bak~";
+              home-manager.useGlobalPkgs = true;
+              home-manager.useUserPackages = true;
+              home-manager.users."gang.liu" = import ./home;
+          }
+       ];
     };
 
     # Expose the package set, including overlays, for convenience.
