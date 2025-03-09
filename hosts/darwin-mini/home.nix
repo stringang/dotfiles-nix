a93479bee8b7d95107697050547acb655aa709ed
gang.liu <14845458+stringang@users.noreply.github.com>
2025-03-09 23:43:13 +0800

    Add java,maven

--- hosts/darwin-mini/home.nix
+++ hosts/darwin-mini/home.nix
@@ -1,4 +1,23 @@
-{lib, pkgs, ...}: {
+{
+  lib,
+  pkgs,
+  ...
+}: {
+  home.packages = with pkgs; [
+    ((maven.override {
+        jdk_headless = jdk8_headless;
+      })
+      .overrideAttrs
+      (finalAttrs: previousAttrs: {
+        version = "3.6.3";
+        src = builtins.fetchurl {
+          url = "https://archive.apache.org/dist/maven/maven-3/3.6.3/binaries/apache-maven-3.6.3-bin.tar.gz";
+          sha256 = "1i9qlj3vy4j1yyf22nwisd0pg88n9qzp9ymfhwqabadka7br3b96";
+          # hash = "sha512-c35a1803a6e70a126e80b2b3ae33eed961f83ed74d18fcd16909b2d44d7dada3203f1ffe726c17ef8dcca2dcaa9fca676987befeadc9b9f759967a8cb77181c0";
+        };
+      }))
+  ];
+
   programs.ssh = {
     enable = true;
     extraConfig = ''
@@ -36,4 +55,8 @@
     };
   };
 
+  programs.java = {
+    enable = true;
+    package = pkgs.jdk8;
+  };
 }
